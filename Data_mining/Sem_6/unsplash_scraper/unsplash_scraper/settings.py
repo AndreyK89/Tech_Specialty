b7e9bb4c42d6cc142930028a4935b9c9b74c7742
AndreyK89 <108906823+AndreyK89@users.noreply.github.com>
2024-03-18 22:39:57 +0500

    Edit HW_6

--- Data_mining/Sem_6/unsplash_scraper/unsplash_scraper/settings.py
+++ Data_mining/Sem_6/unsplash_scraper/unsplash_scraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'unsplash_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'unsplash_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -25,7 +25,8 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -50,9 +51,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'unsplash_scraper.middlewares.UnsplashScraperDownloaderMiddleware': 543,
-#}
+ITEM_PIPELINES = {
+   'unsplash_scraper.pipelines.UnsplashScraperPipeline': 1,
+}
+
+IMAGES_STORE = 'images'
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
